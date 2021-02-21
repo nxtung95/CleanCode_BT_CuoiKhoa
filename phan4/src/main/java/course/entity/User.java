404b00beb47f55415f47e539390540fc5a1f6bfe
TungNX <nxtung95@gmail.com>
2021-02-21 18:31:15 +0700

    Delete old phan4

---
+++ phan4/src/main/java/course/entity/User.java
@@ -0,0 +1,27 @@
+package course.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+    
+    protected final String name;
+    protected final String email;
+    protected final String password;
+}
---
+++ phan4/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+ INSERT INTO users(id, name, email, password) VALUES
+ (1, 'Alice', 'nx@gmail.com', 'Test1234'),
+ (2, 'Blice', 'nx1@gmail.com', 'Test123456');
+ 
+INSERT INTO students(student_id, year) VALUES
+ (1, 3);
+ 
+ INSERT INTO teachers(teacher_id, phone, experiences) VALUES
+ (2, '01245464576', 5);
